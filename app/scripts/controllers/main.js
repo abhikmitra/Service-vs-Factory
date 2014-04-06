515e26461119f910635d00d16586f9ddc026cfd5
abhik.mitra <abhik.mitra@flipkart.com>
2014-04-06 15:04:08 +0530

    Small change to demonstrate static variable

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,10 @@
 'use strict';
 
 angular.module('serviceVsFactoryApp')
-    .controller('MainCtrl', function ($scope, customFactory, Customservice) {
+    .controller('MainCtrl', function ($scope, customFactory, Customservice,customFactoryThatInstantiatesAfunction) {
         console.log("Calling test function on service",Customservice.test());
         console.log("Calling test function on factory",customFactory.test());
+        console.log("Calling test function on factory that returns a function",customFactoryThatInstantiatesAfunction.test());
         console.log("Incrementing variable on service",++Customservice.thisVariable);
         console.log("Incrementing variable on factory",++customFactory.thisVariable);
         customFactory.addingRandom="random text";
--- app/scripts/services/customFactory.js
+++ app/scripts/services/customFactory.js
@@ -15,10 +15,27 @@ angular.module('serviceVsFactoryApp')
             thisVariable : 100
         };
     })
+    .factory('customFactoryThatInstantiatesAfunction', function () {
+
+        console.log("CustomFactory has been Called")
+        var localVariable = 1;
+        console.warn("Value of this inside a factory",this,"Notice the $get")
+        function Customservice() {
+            console.log("CustomService has been Called");
+            console.warn("Value of this inside a factory", this, "Notice the __proto__ by putting a break point")
+            var localVariable = 1;
+            this.thisVariable = 100;
+            this.test = function () {
+
+                return ++localVariable;
+            }
+        }
+        return new Customservice();
+    })
     .factory('customFactoryThatReturnsAFunction', function () {
 
-        this.privateVariable = 1;
-        var a = 1;
+
+        var staticVariable = 1;
 
 
         function customFn(){
@@ -26,6 +43,7 @@ angular.module('serviceVsFactoryApp')
             console.log("custom Function is called")
         }
         customFn.prototype.increment = function(){
+            staticVariable++;
             return ++this.value;
         }
         return customFn
